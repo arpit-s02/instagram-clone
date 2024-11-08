990691072b52c5510a04a4683f99abd87e05c079
Singh <arpit.s.singh@accenture.com>
2024-11-08 09:34:17 +0530

    update user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -17,6 +17,9 @@ const userSchema = new mongoose.Schema(
         },
         profilePicture: {
             type: String
+        },
+        bio: {
+            type: String
         }
     },
     {
