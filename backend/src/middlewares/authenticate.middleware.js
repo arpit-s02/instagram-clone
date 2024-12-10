7c73ae496afc92afb4085bfd6a64d40adc180854
arpit singh <arpitrahul02@gmail.com>
2024-12-10 14:38:28 +0530

    update authentication middleware

--- backend/src/middlewares/authenticate.middleware.js
+++ backend/src/middlewares/authenticate.middleware.js
@@ -17,7 +17,7 @@ const authenticate = async (req, res, next) => {
         const user = await findUserById(id);
 
         if (!user) {
-            throw new ApiError("user not found", StatusCodes.NOT_FOUND);
+            throw new ApiError("Invalid user", StatusCodes.UNAUTHORIZED);
         }
 
         req.user = user;
