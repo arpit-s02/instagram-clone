d9624d6a9518fe1be46cc4c8406111918605f73e
arpit singh <arpitrahul02@gmail.com>
2024-12-12 16:53:28 +0530

    include replies count in comment schema

--- backend/src/models/comment.model.js
+++ backend/src/models/comment.model.js
@@ -1,32 +1,36 @@
 import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema(
-    {
-        content: {
-            type: String,
-            required: true
-        },
-        post: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Post",
-            required: true
-        },
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true
-        },
-        parentId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Comment",
-            default: null
-        }
+  {
+    content: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    parentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+      default: null,
+    },
+    repliesCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Comment = mongoose.model('Comment', commentSchema);
+const Comment = mongoose.model("Comment", commentSchema);
 
 export default Comment;
