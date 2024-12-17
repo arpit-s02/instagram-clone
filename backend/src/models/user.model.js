c895e3d1b41e170a4fc86ddad8b828d9ac1b7eba
arpit singh <arpitrahul02@gmail.com>
2024-12-17 14:29:28 +0530

    include posts count in user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,35 +1,39 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        profilePicture: {
-            type: String,
-        },
-        bio: {
-            type: String,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+    postsCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const User = mongoose.model("User", userSchema);
