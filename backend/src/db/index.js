1cb3dc8fd6265351a3190eba7868946170c12c6f
arpit singh <arpitrahul02@gmail.com>
2024-12-31 16:18:08 +0530

    update db connection error handling

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 import { MONGODB_URI } from "../../config.js";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(MONGODB_URI);
-        console.log("Connected to DB");
-        
-    } catch (error) {
-        console.log("error while connecting to db", error);
-    }
-}
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("Connected to DB");
+  } catch (error) {
+    console.error("Error while connecting to db", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
